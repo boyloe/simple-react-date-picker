d5c95e0242866e7f246c536f4fe13a3e93a17a50
boyloe <boyloe@gmail.com>
2020-11-15 10:59:25 -0700

    Add age calculator

--- src/App.js
+++ src/App.js
@@ -5,22 +5,42 @@ import './App.css'
 
 
 
+
 function App() {
   const [selectedDate, setselectedDate] = useState(new Date())
 
-  return (     
-    <div className="birthdate-input">
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <DatePicker 
-            disableFuture
-            openTo="year"
-            format='dd/MM/yyyy'
-            label='Date of Birth'
-            views={["year", 'month', 'date']}
-            value={selectedDate}
-            onChange={setselectedDate}
-          />  
-      </MuiPickersUtilsProvider>
+  const calculateAge = (dateStr) => {
+      const today = new Date()
+      const birthDate = new Date(dateStr)
+      let age = today.getFullYear() - birthDate.getFullYear()
+      const month = today.getMonth() - birthDate.getMonth()
+      if ( month < 0 || ( month === 0 && today.getDate() < birthDate.getDate())) {
+        age--
+      }
+      return age
+
+  }
+  return (
+    <div className="app">
+      <div className="birthdate-input-container">
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+            <DatePicker 
+              disableFuture
+              openTo="year"
+              format='dd/MM/yyyy'
+              label='Date of Birth'
+              views={["year", 'month', 'date']}
+              value={selectedDate}
+              onChange={(dateObj) => setselectedDate(dateObj._d)}
+            />  
+        </MuiPickersUtilsProvider>
+      </div>
+      <div>
+        <p className="age-display">
+          {`Age: ${calculateAge(selectedDate)}`}
+        </p>
+      </div>
+
     </div>
   );
 
